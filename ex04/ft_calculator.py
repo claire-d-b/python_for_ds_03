43cf701bcee405afbe1172c3ecf16764e3e790dd
claire-d-b <claire.deberny@essca.eu>
2024-07-11 19:48:20 +0200

    end of day 3

---
+++ ex04/ft_calculator.py
@@ -0,0 +1,39 @@
+def dotproduct(func):
+    def wrapper(V1, V2):
+        func(V1, V2)
+    return wrapper
+
+def add_vec(func):
+    def wrapper(V1, V2):
+        func(V1, V2)
+    return wrapper
+    
+def sous_vec(func):
+    def wrapper(V1, V2):
+        func(V1, V2)
+    return wrapper
+
+class calculator:
+
+#your code here
+    @dotproduct
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        res = 0
+        for a,b in zip(V1, V2):
+            res += a * b
+        print(f"Dot product is: {res}")
+    #your code here
+    @add_vec
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        res = []
+        for a,b in zip(V1, V2):
+            res.append(float(a + b))
+        print(f"Add Vector is: {res}")
+    #your code here
+    @sous_vec
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        res = []
+        for a,b in zip(V1, V2):
+            res.append(float(a - b))
+        print(f"Sous Vector is: {res}")
+    #your code here
